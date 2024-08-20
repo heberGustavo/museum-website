2b40347abc5e6c8972946d38e63f28397eb0b7b2
heberGustavo <heber.gbarbosa@hotmail.com>
2024-08-20 10:12:36 -0300

    Menu mobile custom

---
+++ js/mobile.js
@@ -0,0 +1,30 @@
+let burger = document.getElementById('burger'),
+	nav = document.getElementById('main-nav');
+
+$(document).ready(function () {
+	var widthPage = window.innerWidth;
+	if (widthPage <= 991) {
+		$("header .container-menu-mobile").removeClass("d-none");
+	}
+});
+
+burger.addEventListener('click', function (e) {
+	this.classList.toggle('is-open');
+	nav.classList.toggle('is-open');
+});
+
+/* Onload demo - dirty timeout */
+let clickEvent = new Event('click');
+
+window.addEventListener('load', function (e) {
+	slowmo.dispatchEvent(clickEvent);
+	burger.dispatchEvent(clickEvent);
+
+	setTimeout(function () {
+		burger.dispatchEvent(clickEvent);
+
+		setTimeout(function () {
+			slowmo.dispatchEvent(clickEvent);
+		}, 3500);
+	}, 5500);
+});
