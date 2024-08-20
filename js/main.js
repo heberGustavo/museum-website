aafc24e8584e04ac474026f72184bc54b5d96802
heberGustavo <heber.gbarbosa@hotmail.com>
2024-08-20 09:40:31 -0300

    Modifications for mobile

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
+$(document).ready(function() {
+    var widthPage = $(window).width();
+    if(widthPage <= 1198){
+        $(".newsletter .form").removeClass("col-md-6").addClass("row");
+    }
+});
+
+
+
 $(window).scroll(function () {
     var scroll = $(window).scrollTop();
-    console.log(scroll)
+    
     if (scroll >= 100) {
         $("header").addClass("bg-black", 500);
     }
